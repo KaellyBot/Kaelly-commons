d0756f1da0af2d383ecc755e400bca38f5455416
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-10-01 13:47:59 +0200

    Game logo/icon added. Guild moved to core project.

--- src/main/java/com/github/kaellybot/commons/model/constants/Game.java
+++ src/main/java/com/github/kaellybot/commons/model/constants/Game.java
@@ -7,11 +7,13 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Game {
 
-    DOFUS("Dofus", "https://static.ankama.com/g/modules/masterpage/block/header/navbar/dofus/logo.png"),
-    DOFUS_TOUCH("Dofus Touch", "https://static.ankama.com/g/modules/masterpage/block/header/navbar/dofus-touch/logo.png"),
-    DOFUS_RETRO("Dofus Retro", "https://static.ankama.com/dofus/ng/modules/mmorpg/retro/new/assets/logo.png");
+    DOFUS("Dofus"            , "https://static.ankama.com/g/modules/masterpage/block/header/navbar/dofus/logo.png"      , "https://i.imgur.com/gJMIxvo.png"),
+    DOFUS_TOUCH("Dofus Touch", "https://static.ankama.com/g/modules/masterpage/block/header/navbar/dofus-touch/logo.png", "https://i.imgur.com/D9ArwwF.png"),
+    DOFUS_RETRO("Dofus Retro", "https://static.ankama.com/dofus/ng/modules/mmorpg/retro/new/assets/logo.png"            , "https://i.imgur.com/ipYjRAo.png");
 
     private final String name;
 
-    private final String image;
+    private final String logo;
+
+    private final String icon;
 }
--- src/main/java/com/github/kaellybot/commons/repository/GuildRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.kaellybot.commons.repository;
-
-import com.github.kaellybot.commons.model.entity.Guild;
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
-
-public interface GuildRepository extends ReactiveMongoRepository<Guild, String> {
-}
