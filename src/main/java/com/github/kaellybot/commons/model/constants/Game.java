0b11eb2107b5adff41dd4ee7499f862e8735597d
Steven Pescheteau <steven.pescheteau@gmail.com>
2020-10-01 11:42:46 +0200

    Name and icon added to Game enumeration

--- src/main/java/com/github/kaellybot/commons/model/constants/Game.java
+++ src/main/java/com/github/kaellybot/commons/model/constants/Game.java
@@ -1,5 +1,17 @@
 package com.github.kaellybot.commons.model.constants;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Game {
-    DOFUS, DOFUS_TOUCH, DOFUS_RETRO
+
+    DOFUS("Dofus", "https://static.ankama.com/g/modules/masterpage/block/header/navbar/dofus/logo.png"),
+    DOFUS_TOUCH("Dofus Touch", "https://static.ankama.com/g/modules/masterpage/block/header/navbar/dofus-touch/logo.png"),
+    DOFUS_RETRO("Dofus Retro", "https://static.ankama.com/dofus/ng/modules/mmorpg/retro/new/assets/logo.png");
+
+    private final String name;
+
+    private final String image;
 }
